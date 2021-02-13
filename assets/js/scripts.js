4e4240b38980e9d80fd4f18e5be5b1e0d6083239
OliHickie <oli.h@hotmail.co.uk>
2021-02-13 09:53:21 +0000

    created table for each country

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,16 @@
 $(document).ready(function () {
     
     $(".country-btn").click(function () {
+        // Keep buttons active once clicked
         $(".country-btn").children("img").removeClass("country-btn-active");
         $(this).children("img").addClass("country-btn-active");
+
+
+        // var country = $(this).attr("id")
+        // var newCountry = `.${country}`
+
+        // console.log(newCountry);
+
     })
+
 });
