9fc0a92000031f58b85ea2169495e5d3767ced69
OliHickie <oli.h@hotmail.co.uk>
2021-02-13 12:59:42 +0000

    embedded skyscanner widget

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,24 @@
-$(document).ready(function () {
-    
-    $(".country-btn").click(function () {
-        // Keep buttons active once clicked
-        $(".country-btn").children("img").removeClass("country-btn-active");
-        $(this).children("img").addClass("country-btn-active");
+// $(document).ready(function () {
+function openPopUp() {
+    document.getElementById("form-display").style.display = "block";
+}
+
+function closePopUp() {
+    document.getElementById("form-display").style.display = "none";
+}
 
 
-        // var country = $(this).attr("id")
-        // var newCountry = `.${country}`
+$(".country-btn").click(function () {
+    // Keep buttons active once clicked
+    $(".country-btn").children("img").removeClass("country-btn-active");
+    $(this).children("img").addClass("country-btn-active");
 
-        // console.log(newCountry);
 
-    })
+    // var country = $(this).attr("id")
+    // var newCountry = `.${country}`
 
-});
+    // console.log(newCountry);
+
+})
+
+// });
