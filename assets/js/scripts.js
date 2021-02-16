f7888d2d47414d3ec3dc75e20532cf4773f29ff3
OliHickie <oli.h@hotmail.co.uk>
2021-02-16 11:18:55 +0000

    moved country buttons to scripts.js and positioned within hero img

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,43 @@
-$("document").ready(function(){
-    $(".information").hide(); //hide all information when page loads
+const countries = ["england", "wales", "scotland", "ireland", "france", "italy"]
+
+const countriesMap = () => {
+    countries.map((country) => {
+        $("#countries").append(
+            `<div class="col-4 col-sm-2 country-btn-container">
+                <button class="country-btn btn-hover" id="${country}"><img src="assets/images/flags/${country}-flag.png"
+                        alt="${country} Flag" class="flag d-none">${country.charAt(0).toUpperCase() + country.slice(1)}</button>
+            </div>`);
+    })
+}
+
+const countrySelector = (function () {
+    $(".country-btn").click(function () {
+
+        if ($(this).children("img").hasClass("d-none")) {
+            $(".country-btn").children("img").addClass("d-none"),
+                $(this).children("img").removeClass("d-none");
+        } else {
+            $(".country-btn").children("img").addClass("d-none");
+        }
+
+        $(".information").hide();
+        var selectedCountry = `#${this.id}-info`;
+        $(selectedCountry).show();
+    });
 })
 
+
+$("document").ready(function () {
+    $(".information").hide(); //hide all information when page loads
+
+    countriesMap();
+    countrySelector();
+})
+
+
 // Add background color to dropdown menu
-$(".navbar-toggler").click(function() {
-    
+$(".navbar-toggler").click(function () {
+
     if ($(".navbar").hasClass("toggler-bg")) {
         $(".navbar").removeClass("toggler-bg");
     } else {
@@ -12,25 +45,16 @@ $(".navbar-toggler").click(function() {
     }
 })
 
-$(".country-btn").click(function () {
 
-    // Keep buttons active once clicked
-    if ($(this).children("img").hasClass("country-btn-active")) {
-        $(".country-btn").children("img").removeClass("country-btn-active");
-    } else {
-        $(".country-btn").children("img").removeClass("country-btn-active");
-        $(this).children("img").addClass("country-btn-active");
-    }
 
-    // Hide/show country's information/map
-    $(".information").hide();
+//     // Hide/show country's information/map
+$(".information").hide();
 
-    var selectedCountry = `#${this.id}-info`;
+var selectedCountry = `#${this.id}-info`;
 
-    $(selectedCountry).show();
-})
+$(selectedCountry).show();
 
-// 
+
 
 
 
